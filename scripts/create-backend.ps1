69c6fc2b2307ef8cd64c6e0b178cf826bfa3b986
PadarbindaPuhan <Padarbinda.Puhan@gmail.com>
2025-05-10 11:47:40 +0530

    push to CI branch

--- scripts/create-backend.ps1
+++ scripts/create-backend.ps1
@@ -8,9 +8,16 @@ try{
     
     $bucketInfo= Get-S3Bucket -BucketName $bucketName
     
+    if($bucketInfo){
+        Write-Host "Bucket $bucketName exists"
+    }
+    else{
+        Write-Host "Creating new bucket $bucketName"
+    }
 }
 catch{
     Write-Host "Error creating bucket"+$_.message
+    exit(1)
 }
 
 

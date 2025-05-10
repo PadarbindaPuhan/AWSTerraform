629d8ee9e991fc0433907f61c25976613f5caeab
PadarbindaPuhan <Padarbinda.Puhan@gmail.com>
2025-05-10 12:55:56 +0530

    push to CI branch

--- scripts/create-backend.ps1
+++ scripts/create-backend.ps1
@@ -12,7 +12,8 @@ param(
 try{
     Import-Module AWSPowerShell.NetCore
     
-    $bucketInfo= Get-S3Bucket -BucketName $bucketName
+    Write-Host "Check if bucket $bucketName exists in region $region and create it if not"
+    $bucketInfo= Get-S3Bucket -BucketName $bucketName 
     
     if($bucketInfo){
         Write-Host "Bucket $bucketName exists"
@@ -22,6 +23,19 @@ try{
         Import-Module ./create-backend-bucket.psm1
         New-BackendBucket -bucketName $bucketName -region $region
     }
+
+    Write-Host "Check if DynamoDB table $dynamoDbTableName exists in region $region and create it if not"
+
+    $dynamoDbTableInfo= Get-DDBTable -TableName $dynamoDbTableName
+    if($dynamoDbTableInfo){
+        Write-Host "DynamoDB table $dynamoDbTableName exists"
+    }
+    else{
+        Write-Host "Creating new DynamoDB table $dynamoDbTableName"
+        Import-Module ./create-backend-backend-dynamodb.psm1
+        New-BackendDynamoDBTable -tableName $dynamoDbTableName -region $region
+    }
+
 }
 catch{
     Write-Host "Error creating bucket"+$_.message

751bb93aa5554eab59a6c5e1a155f4793dc21c06
PadarbindaPuhan <Padarbinda.Puhan@gmail.com>
2025-05-11 21:16:07 +0530

    fixed ports for security group

--- infrastructure/modules/securitygroup/main.tf
+++ infrastructure/modules/securitygroup/main.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "allow_all" {
   ingress {
     protocol    = "-1"  # "-1" means all protocols
     cidr_blocks = var.cidr_blocks
-    to_port = -1  # "-1" means all ports
-    from_port = -1  # "-1" means all ports
+    to_port = 0  # "0" means all ports
+    from_port = 0  # "0" means all ports
     
   }
   
